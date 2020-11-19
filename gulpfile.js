5950823eb665d233552e901c72bcb923e82d6681
mitsui <m.mihye1128@gmail.com>
2020-11-19 19:54:11 +0900

    add gulp-dart-sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,16 @@
 const { src, dest } = require('gulp');
+const sass = require('gulp-dart-sass');
+
+const paths = {
+  scss: './src/scss/**/*.scss',
+};
+
+const css = () => {
+  return src(paths.scss, { sourcemaps: true })
+    .pipe(sass({
+      outputStyle: 'expanded'
+    }).on('error', sass.logError))
+    .pipe(dest('dist/assets/css/', { sourcemaps: '.' }));
+};
+
+exports.css = css;
