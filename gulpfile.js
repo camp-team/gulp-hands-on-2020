b9c0b1fa5ba59ada875f3146901ce665b48e7a20
mitsui <m.mihye1128@gmail.com>
2020-11-19 20:11:54 +0900

    add postcss

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,35 @@
 const { src, dest } = require('gulp');
+
 const sass = require('gulp-dart-sass');
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
+const flexBugsFixes = require('postcss-flexbugs-fixes');
+const declarationSorter = require('css-declaration-sorter');
+const cssWring = require('csswring');
 
 const paths = {
   scss: './src/scss/**/*.scss',
 };
+
+const autoprefixerOption = {
+  grid: true
+};
+const declarationSorterOption = {
+  order: 'smacss'
+};
+const postcssOption = [
+  flexBugsFixes,
+  autoprefixer(autoprefixerOption),
+  declarationSorter(declarationSorterOption),
+  cssWring
+];
 
 const css = () => {
   return src(paths.scss, { sourcemaps: true })
     .pipe(sass({
       outputStyle: 'expanded'
     }).on('error', sass.logError))
+    .pipe(postcss(postcssOption))
     .pipe(dest('dist/assets/css/', { sourcemaps: '.' }));
 };
 
