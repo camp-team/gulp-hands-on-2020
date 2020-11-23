8832127ff60fd62f039d896987230cb6d3a5668e
mitsui <m.mihye1128@gmail.com>
2020-11-23 21:48:27 +0900

    開発環境用ファイルの整理

--- README.md
+++ README.md
@@ -1,36 +1,19 @@
-# Gulpハンズオン
+# Gulpハンズオン サンプルコード
 
-## 弟1回
-### 日時
-2020/11/19(木) 21:00 ~ 23:00
+2020/11/26(木)に行うGulpハンズオンで使用するリポジトリです。
+前回（11/19）の勉強会に参加できなかった方は、こちらをcloneして参加してください。
 
-### 進行
-@mihye1128
+## clone後
+```
+$ npm i
+```
 
-### お品書き
-- なぜこのタイミングでGulp勉強会をしようと思ったのか
-- gulpfile.jsの中身をざっくり解説
-- ハンズオン
+## dev （ホットリロード）
+```
+$ npm gulp
+```
 
-## インストールするパッケージ一覧
-- [gulp](https://www.npmjs.com/package/gulp)
-
-- [gulp-dart-sass](https://www.npmjs.com/package/gulp-dart-sass)
-- [gulp-postcss](https://www.npmjs.com/package/gulp-postcss)
-- [autoprefixer@9.8.6](https://www.npmjs.com/package/autoprefixer)
-- [postcss-flexbugs-fixes@4.2.1](https://www.npmjs.com/package/postcss-flexbugs-fixes)
-- [css-declaration-sorter@5.1.2](https://www.npmjs.com/package/css-declaration-sorter)
-- [csswring@7.0.0](https://www.npmjs.com/package/csswring)
-
-- [gulp-pug](https://www.npmjs.com/package/gulp-pug)
-- [gulp-plumber](https://www.npmjs.com/package/gulp-plumber)
-- [gulp-notify](https://www.npmjs.com/package/gulp-notify)
-
-- [webpack](https://www.npmjs.com/package/webpack)
-- [webpack-stream](https://www.npmjs.com/package/webpack-stream)
-
-- [gulp-imagemin](https://www.npmjs.com/package/gulp-imagemin)
-- [imagemin-pngquant](https://www.npmjs.com/package/imagemin-pngquant)
-- [imagemin-mozjpeg](https://www.npmjs.com/package/imagemin-mozjpeg)
-
-- [browser-sync](https://www.npmjs.com/package/browser-sync)
+## build
+```
+$ npx gulp build
+```
--- gulpfile.js
+++ gulpfile.js
@@ -1,53 +1,30 @@
-// パッケージパッケージの読み込み
 const { src, dest, parallel, series, watch } = require('gulp');
-
 const plumber = require('gulp-plumber');
 const notify = require('gulp-notify');
-
 const pug = require('gulp-pug');
-
 const sass = require('gulp-dart-sass');
 const postcss = require('gulp-postcss');
 const autoprefixer = require('autoprefixer');
 const flexBugsFixes = require('postcss-flexbugs-fixes');
 const declarationSorter = require('css-declaration-sorter');
 const cssWring = require('csswring');
-
 const browserSync = require('browser-sync').create();
 
-// オプション設定
 const paths = {
   pug: './src/pug/**/*.pug',
   scss: './src/scss/**/*.scss',
 };
 
-const pugOption = {
-  basedir: './src/pug',
-  pretty: true // みやすいコードでの出力
-}
-
-const autoprefixerOption = {
-  grid: true
-};
-const declarationSorterOption = {
-  order: 'smacss'
-};
-const postcssOption = [
-  flexBugsFixes,
-  autoprefixer(autoprefixerOption),
-  declarationSorter(declarationSorterOption),
-  cssWring
-];
-
-const browserSyncOption = {
-  server: './dist'
-};
-
-// Gulpタスク 定義
 const html = () => {
-  return src([paths.pug, '!**/*_*'])
+  return src([
+      paths.pug,
+      '!**/*_*'
+    ])
     .pipe(plumber({ errorHandler: notify.onError("Error: <%= error.message %>") }))
-    .pipe(pug(pugOption))
+    .pipe(pug( {
+      basedir: './src/pug',
+      pretty: true
+    }))
     .pipe(dest('dist/'))
 };
 
@@ -56,7 +33,16 @@ const css = () => {
     .pipe(sass({
       outputStyle: 'expanded'
     }).on('error', sass.logError))
-    .pipe(postcss(postcssOption))
+    .pipe(postcss([
+      flexBugsFixes,
+      autoprefixer({
+        grid: true
+      }),
+      declarationSorter({
+        order: 'smacss'
+      }),
+      cssWring
+    ]))
     .pipe(dest('dist/assets/css/', { sourcemaps: '.' }))
     .pipe(browserSync.stream());;
 };
@@ -70,23 +56,23 @@ const watchFiles = () => {
     css();
     cb();
   });
-  watch(['./dist/**/*.html'], function(cb) {
+  watch('./dist/**/*.html', function(cb) {
     browserSync.reload()
     cb();
   });
 }
 
 const server = () => {
-  browserSync.init(browserSyncOption);
+  browserSync.init({
+    server: './dist'
+  });
 };
 
 const build = parallel(html, css);
 
-// Gulpタスク エクスポート
 exports.html = html;
 exports.css = css;
 exports.watchFiles = watchFiles;
-
 exports.build = build;
 exports.default = series(
   build,
