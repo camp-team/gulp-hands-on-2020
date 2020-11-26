f93e7efbdc339866a795b0293f856960c4ce55d3
mitsui <m.mihye1128@gmail.com>
2020-11-26 21:49:01 +0900

    feat(gulp):jsタスクの作成

--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,8 @@ import gnav from './modules/gnav';
 import smoothScroll from './modules/smoothScroll';
 import fadeInPagetopBtn from './modules/fadein';
 
+console.log('test');
+
 gnav();
 smoothScroll();
 fadeInPagetopBtn();
